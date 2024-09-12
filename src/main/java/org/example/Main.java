6f5b248682159d2536dcc86b594f338433e4bfb4
Tarasov Oleg <tarasovom@mail.ru>
2024-09-12 07:08:22 +0300

    second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,27 @@
 package org.example;
 
+import org.example.Server;
+
+import java.io.BufferedOutputStream;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
 public class Main {
 
     public static void main(String[] args) {
+
         Server server = new Server(64);
-        server.start(9999);
+
+        server.addHandler("GET", "/messages", new Handler() {
+
+        });
+        server.addHandler("POST", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                // TODO: handlers code
+            }
+        });
+
+        server.listen(9999);
     }
+}
 }
