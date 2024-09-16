389500898be34096e13f9fd52df2cba89a3618b3
Tarasov Oleg <tarasovom@mail.ru>
2024-09-16 09:12:43 +0300

    remuve

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,13 @@
 package org.example;
 
-import org.example.Server;
-
-import java.io.BufferedOutputStream;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
-
 public class Main {
+    final static int PORT = 9999;
 
     public static void main(String[] args) {
 
-        Server server = new Server(64);
+        final var server = new Server();
+        server.listen(PORT);
 
-        server.addHandler("GET", "/messages", new Handler() {
-
-        });
-        server.addHandler("POST", "/messages", new Handler() {
-            public void handle(Request request, BufferedOutputStream responseStream) {
-                // TODO: handlers code
-            }
-        });
-
-        server.listen(9999);
     }
 }
-}
+
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,51 +1,27 @@
 package org.example;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
+    final int COUNT = 64;
 
-    private final static Map<String, Map<String, Handler>> handlers = new ConcurrentHashMap<>();
-    ExecutorService executorService;
+    ExecutorService executorService = Executors.newFixedThreadPool(COUNT);
 
-    public Server(int poolSize) {
-        this.executorService = Executors.newFixedThreadPool(poolSize);
-    }
-
-    public void listen (int port) {
+    public void listen(int port) {
 
         try (final var serverSocket = new ServerSocket(port)) {
 
             while (true) {
                 final var socket = serverSocket.accept();
-                executorService.submit(() -> connect(socket));
+                System.out.println(socket);
+                Handler handler = new Handler(socket);
+                executorService.submit(handler);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-    public void addHandler(String method, String path, Handler handler) {
-
-        if (handlers.containsKey(method)) {
-            handlers.get(method).put(path, handler);
-        } else {
-            handlers.put(method, new ConcurrentHashMap<>(Map.of(path, handler)));
-        }
-
-        System.out.println(handlers);
-
-
     }
 }
