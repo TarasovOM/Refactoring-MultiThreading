182b98c4d7b9a7ee39de64545f15d50870d41866
Tarasov Oleg <tarasovom@mail.ru>
2024-09-16 09:08:14 +0300

    query

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,41 +1,36 @@
 package org.example;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
+    final int COUNT = 64;
 
     private final static Map<String, Map<String, Handler>> handlers = new ConcurrentHashMap<>();
-    ExecutorService executorService;
 
-    public Server(int poolSize) {
-        this.executorService = Executors.newFixedThreadPool(poolSize);
-    }
+    ExecutorService executorService = Executors.newFixedThreadPool(COUNT);
 
-    public void listen (int port) {
+    public void listen(int port) {
 
         try (final var serverSocket = new ServerSocket(port)) {
 
             while (true) {
                 final var socket = serverSocket.accept();
-                executorService.submit(() -> connect(socket));
+                System.out.println(socket);
+                ClientHandler clientHandler = new ClientHandler(socket);
+                executorService.submit(clientHandler);
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
+
     public void addHandler(String method, String path, Handler handler) {
 
         if (handlers.containsKey(method)) {
@@ -46,6 +41,9 @@ public class Server {
 
         System.out.println(handlers);
 
+    }
 
+    public static Map<String, Map<String, Handler>> getHandlers() {
+        return handlers;
     }
 }
